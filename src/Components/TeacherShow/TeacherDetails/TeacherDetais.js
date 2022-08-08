2bce73864d921778138baaaa740551e50bde7c38
ifzal22 <enjoyyourlife2244@gmailm>
2022-08-08 16:58:38 +0600

    'push'

--- src/Components/TeacherShow/TeacherDetails/TeacherDetais.js
+++ src/Components/TeacherShow/TeacherDetails/TeacherDetais.js
@@ -1,67 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Header from '../../Home/Header/Header';
-import './TeacherDetails.css';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../../Home/Header/Header";
+import "./TeacherDetails.css";
 
 const TeacherDetais = () => {
+  const { about } = useParams();
+  const [singleData, setSingleData] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/teacher/teacher/${about}`)
+      .then((res) => res.json())
+      .then((data) => setSingleData(data));
+  }, []);
 
-    const {about} = useParams();
-    const [singleData, setSingleData] = useState({});
-    useEffect(() => {
-        fetch(`http://localhost:5000/teacher/teacher/${about}`)
-            .then(res => res.json())
-            .then(data => setSingleData(data))
-    }, [])
-
-
-
-    return (<>
-    
-    <Header></Header>
-    <div className='container marg'>
-        
+  return (
+    <>
+      <Header></Header>
+      <div className="container marg">
         <section className="reviews" id="reviews">
+          <h1 className="heading">
+            {" "}
+            JIM <span>TEACHER'S</span>{" "}
+          </h1>
 
-<h1 className="heading"> JIM <span>TEACHER'S</span> </h1>
-
-<div className="swiper review-slider d-flex justify-content-center ">
-
-
-
-
-
-      <div className=" box col-md-4 shadow-lg CONT">
-    
-     <img src={singleData?.image} alt=""/>
-          <div className="content">
-
-              <h3>{singleData?.cetagory} </h3>
-              <h6>{singleData?.email} </h6>
-              <h6>{singleData?.phone} </h6>
-              <p>{singleData.about} .</p>
-              <h3>{singleData.name} </h3>
-              <div className="stars">
-              <div className='text-center '>    
-              
-         </div>
+          <div className="swiper review-slider d-flex justify-content-center ">
+            <div className=" box col-md-4 shadow-lg CONT">
+              <img src={singleData?.image} alt="" />
+              <div className="content">
+                <h3>{singleData?.cetagory} </h3>
+                <h6>{singleData?.email} </h6>
+                <h6>{singleData?.phone} </h6>
+                <p>{singleData.about} .</p>
+                <h3>{singleData.name} </h3>
+                <div className="stars">
+                  <div className="text-center "></div>
+                </div>
               </div>
+            </div>
+
+            <div className="swiper-pagination"></div>
           </div>
-          </div> 
-
-
-
-
-
-
-
-
-<div className="swiper-pagination"></div>
-
-</div>
-
-</section>
-    </div></>
-    );
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default TeacherDetais;
